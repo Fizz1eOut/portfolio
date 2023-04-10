b5bb2222923fd1cac1c5bacf0043aac8e083eb32
Fizz1eOut <123012547+Fizz1eOut@users.noreply.github.com>
2023-04-10 18:02:27 +0200

    Add files via upload

---
+++ js/magic-line.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const nav = document.querySelector('.nav'),
+          navLine = document.querySelector('.nav__line'),
+          navItem = document.querySelectorAll('.nav__item');
+
+    navLine.style.width = `${navItem[0].offsetWidth}px`;
+
+    navItem.forEach(el => {
+        el.addEventListener('mouseenter', (e) => {
+            navLine.style.width = `${e.currentTarget.offsetWidth}px`;
+            navLine.style.left = `${e.currentTarget.offsetLeft}px`;
+        });
+    });
+});
