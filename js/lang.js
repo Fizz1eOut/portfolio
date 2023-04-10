d05b92df831b8158a2ca015b97cacd5dfe12babb
Fizz1eOut <123012547+Fizz1eOut@users.noreply.github.com>
2023-04-10 11:28:20 +0200

    Add files via upload

---
+++ js/lang.js
@@ -0,0 +1,32 @@
+const langArr = {
+    "profession" :  {
+        "ru": "веб-разработчик",
+        "en": "web-developer",
+        "ua": "веб-розробник",
+    }, 
+    "name": {
+        "ru": "Стешин Андрей",
+        "en": "STESHYN ANDREY",
+        "ua": "Стешин Андрій",
+    }, 
+    "about": {
+        "ru": "Обо мне",
+        "en": "About me",
+        "ua": "Про мене",
+    }, 
+    "skills": {
+        "ru": "Навыки",
+        "en": "Skills",
+        "ua": "Навички",
+    }, 
+    "projects": {
+        "ru": "Проекты",
+        "en": "Projects",
+        "ua": "Проекти",
+    }, 
+    "more": {
+        "ru": "детально о товаре",
+        "en": "more details",
+        "ua": "детально про товар",
+    }, 
+}
