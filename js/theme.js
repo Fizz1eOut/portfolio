4a9e884905d7f9c3eda892b55e400d0df6ad3e12
Fizz1eOut <123012547+Fizz1eOut@users.noreply.github.com>
2023-04-11 19:36:23 +0200

    Add files via upload

---
+++ js/theme.js
@@ -0,0 +1,25 @@
+document.querySelector('.themetoggle').addEventListener('click', (event) => {
+    event.preventDefault();
+    if (localStorage.getItem('theme') === 'light') {
+      localStorage.removeItem('theme');
+    }
+    else {
+      localStorage.setItem('theme', 'light')
+    }
+    addDarkClassToHTML()
+  });
+  
+  function addDarkClassToHTML() {
+    try {
+      if (localStorage.getItem('theme') === 'light') {
+        document.querySelector('html').classList.add('light');
+        document.querySelector('.themetoggle span').textContent = 'dark_mode';
+      }
+      else {
+        document.querySelector('html').classList.remove('light');
+        document.querySelector('.themetoggle span').textContent = 'light_mode';
+      }
+    } catch (err) { }
+  }
+  
+  addDarkClassToHTML();
