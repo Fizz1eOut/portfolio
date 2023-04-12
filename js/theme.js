b827ebb02515cf95aceb8e30e5925d859e604a15
Fizz1eOut <123012547+Fizz1eOut@users.noreply.github.com>
2023-04-12 19:00:59 +0200

    Add files via upload

--- js/theme.js
+++ js/theme.js
@@ -13,11 +13,11 @@ document.querySelector('.themetoggle').addEventListener('click', (event) => {
     try {
       if (localStorage.getItem('theme') === 'light') {
         document.querySelector('html').classList.add('light');
-        document.querySelector('.themetoggle span').textContent = 'dark_mode';
+        // document.querySelector('.themetoggle span').textContent = 'dark_mode';
       }
       else {
         document.querySelector('html').classList.remove('light');
-        document.querySelector('.themetoggle span').textContent = 'light_mode';
+        // document.querySelector('.themetoggle span').textContent = 'light_mode';
       }
     } catch (err) { }
   }
