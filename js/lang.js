6f4338bd107be34bf781480ebaf5d1be10377c30
Fizz1eOut <123012547+Fizz1eOut@users.noreply.github.com>
2023-04-13 19:08:14 +0200

    Add files via upload

--- js/lang.js
+++ js/lang.js
@@ -23,10 +23,40 @@ const langArr = {
         "ru": "Проекты",
         "en": "Projects",
         "ua": "Проекти",
+    },
+    "title-about": {
+        "ru": "Обо мне",
+        "en": "About me",
+        "ua": "Про мене",
     }, 
-    "more": {
-        "ru": "детально о товаре",
-        "en": "more details",
-        "ua": "детально про товар",
+    "about-text": {
+        "ru": "Здравствуйте, меня зовут Андрей, я занимаюсь разработкой сайтов. Занимаюсь версткой, адаптацией и интеграцией. Свою работу я выполняю с учетом современных требований и с использованием лучших технологий.",
+        "en": "Hello, my name is Andrey, I am developing websites. I am engaged in layout, adaptation and integration. I carry out my work taking into account modern requirements and using the best technologies.",
+        "ua": "Привіт, мене звати Андрій, займаюся розробкою сайтів. Займаюся версткою, адаптацією та інтеграцією. Свою роботу виконую з урахуванням сучасних вимог та використовуючи найкращі технології.",
+    },
+    "about-ready": {
+        "ru": "Готов начать прямо сейчас",
+        "en": "Ready to start already",
+        "ua": "Готовий почати прямо зараз",
     }, 
+    "about-always": {
+        "ru": "Я всегда довожу дело до конца",
+        "en": "I always bring things to an end",
+        "ua": "Я завжди доводжу справу до кінця",
+    }, 
+    "about-deadlines": {
+        "ru": "Не нарушаю дедлайнов",
+        "en": "I do not violate deadlines",
+        "ua": "Не порушую дедлайнів",
+    }, 
+    "about-touch": {
+        "ru": "Всегда на связи",
+        "en": "Always in touch",
+        "ua": "Завжди на зв'язку",
+    },
+    "about-button": {
+        "ru": "Контакты",
+        "en": "Contacts",
+        "ua": "Контакти",
+    },
 }
