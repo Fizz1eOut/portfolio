6c14f45e5d685e50fe781e5a6ac948c9841b7279
Fizz1eOut <123012547+Fizz1eOut@users.noreply.github.com>
2023-06-08 19:50:54 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -5,25 +5,23 @@ select.addEventListener('change', changeURLLanguage);
 
 // перенаправить на url с указанием языка
 function changeURLLanguage() {
-    let lang = select.value;
-    location.href = window.location.pathname + '#' + lang;
+    window.location.hash = `#${select.value}`;
     location.reload();
 }
 
 function changeLanguage() {
     const hash = window.location.hash.slice(1);
-    if (!allLang.includes(hash)) {
-        location.href = window.location.pathname + '#en';
-    }
-    select.value = hash;
+    const lang = allLang.includes(hash) ? hash : allLang[0];
+
+    select.value = lang;
+    window.location.hash = `#${lang}`;
     // document.querySelector('title').innerHTML = langArr['unit'][hash];
     // document.querySelector('.lng-chip').innerHTML = langArr['chip'][hash];
-    for (let key in langArr) {
-        let elem = document.querySelector('.lng-' + key);
-        if (elem) {
-            elem.innerHTML = langArr[key][hash];
-        }
 
+    for (let key in langArr) {
+        document.querySelectorAll(`.lng-${key}`).forEach((element) => {
+            element.innerHTML = langArr[key][lang];
+        });
     }
 }
 
